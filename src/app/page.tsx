c06a5f503a969c559eb055f9e4003591bdbc8ffd
erik telemarrk <telemarrk@gmail.com>
2025-09-25 13:27:49 +0000

    Peux-tu enlever dans la liste déroulante de la page login le SG - devant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import * as React from "react";
@@ -91,7 +92,7 @@ export default function LoginPage() {
                         <SelectContent>
                           {orderedServices.map((service) => (
                             <SelectItem key={service.id} value={service.id}>
-                              {service.name}
+                              {service.name.replace('SG - ', '')}
                             </SelectItem>
                           ))}
                         </SelectContent>
